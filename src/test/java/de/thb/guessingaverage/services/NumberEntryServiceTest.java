5116afff3f3eb02ec1848a0da9964c8292028cde
sxBUTCHxs <leonschoenberg@gmx.de>
2024-01-11 09:46:28 +0100

    - refactors the test structure and uses different files for the different component tests

---
+++ src/test/java/de/thb/guessingaverage/services/NumberEntryServiceTest.java
@@ -0,0 +1,41 @@
+package de.thb.guessingaverage.services;
+
+import de.thb.guessingaverage.controller.form.NumberEntryFormModel;
+import de.thb.guessingaverage.repositories.NumberEntryRepository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class NumberEntryServiceTest {
+
+    @Autowired
+    private NumberEntryService numberEntryService;
+
+    @Autowired
+    private NumberEntryRepository numberEntryRepository;
+
+
+    @Test
+    void test_add_number() {
+        NumberEntryFormModel form = new NumberEntryFormModel();
+        float number = 1.f;
+        form.setNumber(number);
+        numberEntryService.addNumberFromNumberEntryFromModel(form);
+        number = 2.5f;
+        form.setNumber(number);
+        numberEntryService.addNumberFromNumberEntryFromModel(form);
+        Assertions.assertEquals(number, numberEntryRepository.findTopByOrderByIdDesc().getNumber(), "Number should be added to the database.");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        numberEntryRepository.delete(numberEntryRepository.findTopByOrderByIdDesc());
+        numberEntryRepository.delete(numberEntryRepository.findTopByOrderByIdDesc());
+    }
+}
+
